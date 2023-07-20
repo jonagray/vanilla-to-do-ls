2e290a13568e2242b258a4dcd9cbf81a80f771b8
jonagray <jon.a.graybill@gmail.com>
2023-07-20 13:40:56 -0700

    Redux or to do list ordering to keep reordered items from reverting position

--- js/script.js
+++ js/script.js
@@ -1,137 +1,138 @@
-// select everything
-
-// select the to-do form
+// Select everything
 const todoForm = document.querySelector('form');
-
-// select input box
 const todoInput = document.querySelector('form input');
-
-// select <ul>
 const todoItemsList = document.querySelector('ul');
-
-// storage array
 let todos = [];
 
-// function to add event listener on form/listen for submit
+// Function to add event listener on form/listen for submit
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   addTodo(todoInput.value);
 });
 
-// function to add todo item
-const addTodo = (item) => {
-  if (item !== "") {
-    // make a todo object
-    const todo = {
-      id: Date.now(),
-      name: item,
-      completed: false
-    };
-
-    // add it to the todos array
-    todos.push(todo);
-    addToLocalStorage(todos); // this will render them inside the <ul>
-
-    // clear input box
-    todoInput.value = "";
-  }
-};
-
-// function to render todos
+// Function to render todos
 const renderTodos = (todos) => {
   todoItemsList.innerHTML = "";
 
-  // loop through each item inside todos
-  todos.forEach((item) => {
-    // check if item has been completed or not
-    const checked = item.completed ? 'checked' : null;
+  const completedTodos = [];
+  const activeTodos = [];
 
-    // make <li> element and fill it
+  todos.forEach((item) => {
+    const checked = item.completed ? 'checked' : null;
     const li = document.createElement('li');
     li.setAttribute('class', 'item');
     li.setAttribute('data-key', item.id);
+
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${checked}>
+      <span>${item.name}</span>
+      <i class="fa fa-trash delete-button"></i>
+    `;
+
     if (item.completed) {
       li.classList.add('checked');
-
-      li.innerHTML = `
-      <input type="checkbox" class="checkbox" ${checked}>
-      <span>${item.name}</span>
-      <i class="fa fa-trash delete-button"></i>
-      `;
-  
-      todoItemsList.append(li);
+      completedTodos.push(li);
     } else {
-      li.innerHTML = `
-      <input type="checkbox" class="checkbox" ${checked}>
-      <span>${item.name}</span>
-      <i class="fa fa-trash delete-button"></i>
-      `;
-  
-      todoItemsList.prepend(li);
+      activeTodos.push(li);
     }
-
   });
-}
 
-// function to add todos to local storage
-const addToLocalStorage = (todos) => {
+  // Append active todos first, then completed todos
+  activeTodos.forEach((li) => {
+    todoItemsList.appendChild(li);
+  });
 
-  // convert the array to a string, and then store it
-  localStorage.setItem('todos', JSON.stringify(todos));
-
-  // render to screen
-  renderTodos(todos);
+  completedTodos.forEach((li) => {
+    todoItemsList.appendChild(li);
+  });
 };
 
-// function to retreive data from local storage
+// Function to add todos to local storage
+const addToLocalStorage = (todos) => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+// Function to retreive data from local storage and render todos
 const getFromLocalStorage = () => {
   const reference = localStorage.getItem('todos');
   if (reference) {
     todos = JSON.parse(reference);
     renderTodos(todos);
   }
-}
+};
 
-// function to toggle value as completed/not completed
+// Function to update local storage with the new order
+const updateLocalStorageOrder = () => {
+  const updatedOrder = [];
+  const liItems = todoItemsList.querySelectorAll('li');
+
+  liItems.forEach((li) => {
+    const id = li.getAttribute('data-key');
+    const todo = todos.find((item) => item.id.toString() === id);
+    if (todo) {
+      updatedOrder.push(todo);
+    }
+  });
+
+  todos = updatedOrder;
+  addToLocalStorage(todos);
+};
+
+// Function to toggle value as completed/not completed
 const toggle = (id) => {
   todos.forEach((item) => {
-    // using == instead of === because types are different (num vs str)
     if (item.id == id) {
       item.completed = !item.completed;
     }
   });
 
   addToLocalStorage(todos);
+  renderTodos(todos); // Add this line to re-render the updated list immediately
+};
+
+// Function to add todo item
+const addTodo = (item) => {
+  if (item !== "") {
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false
+    };
+
+    todos.push(todo);
+    addToLocalStorage(todos);
+    renderTodos(todos); // Add this line to re-render the updated list
+    todoInput.value = "";
+  }
 };
 
 // Function to delete todo from array, and then update localstorage/rerender
 const deleteTodo = (id) => {
-  // filter out <li> with id
-  todos = todos.filter((item) => {
-    return item.id != id;
-  });
+  // Filter out the todo with the matching id and update the todos array
+  todos = todos.filter((item) => item.id != id);
 
-  // update localstorage
+  // Update localstorage and re-render the updated list
   addToLocalStorage(todos);
+  renderTodos(todos);
 };
 
-// run func to get everything from local storage
+// Run func to get everything from local storage and render todos
 getFromLocalStorage();
 
-// add event listener to the <ul> for checkbox and delete
+// Add event listener to the <ul> for checkbox and delete
 todoItemsList.addEventListener('click', (e) => {
   if (e.target.type === 'checkbox') {
-    // toggle the state
     toggle(e.target.parentElement.getAttribute('data-key'));
   }
 
-  // check if deleting
   if (e.target.classList.contains('delete-button')) {
-    // get id from data-key attribute's value of parent <li> where delete-button is present
     deleteTodo(e.target.parentElement.getAttribute('data-key'));
   }
 });
 
+// Enable sortable functionality and update local storage after reordering
 $(function() {
-  $("#sortable").sortable();
+  $("#sortable").sortable({
+    update: updateLocalStorageOrder
+  });
 });
